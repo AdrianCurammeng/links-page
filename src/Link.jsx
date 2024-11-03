9807cb123ca4434a2b976675fb5b9c9eb0424b53
Adrian Curammeng <adrianvcurammeng@gmail.com>
2024-11-03 01:40:59 -0500

    Feature: Added clickable link buttons
    - Added buttons that can be clicked to go to the link
    - TODO: Begin styling website
    - TODO: Choose font/decide font size

--- src/Link.jsx
+++ src/Link.jsx
@@ -3,15 +3,11 @@ import linksJson from './links.json'
 function Link({ linkItem }) {
     return(
         <>
-
-            <div className="w-full h-full bg-purple-500
-                            m-1 px-4">
-                <a className="w-full h-full p-2" href={linkItem.link}>
-                    <div className="flex flex-row justify-between">
-                        <p>logo</p>
-                        {linkItem.name}
-                        <p>logo</p>
-                    </div>
+            <div className="w-full h-full bg-green-500 my-2">
+                <a className="flex flex-row justify-between h-full w-full p-6" href={linkItem.link}>
+                    <p>Icon</p>
+                    <p>{linkItem.name}</p>
+                    <p>Icon</p>
                 </a>
             </div>
         </>
