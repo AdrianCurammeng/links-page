f14207831b0860b18675b5ee5b1654c1cb02bb82
Adrian Curammeng <adrianvcurammeng@gmail.com>
2024-11-03 01:40:27 -0400

    Feature: Basic website formatting
    - Added some base website formatting to refine later
    - TODO: Decide on font sizes/font choice
    - TODO: Refine formatting

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,10 +5,15 @@ import data from './links.json'
 function Content() {
     return(
         <>
-            <Header/>
-            {data.links.map((item, key) => (
-                <Link linkItem={item}></Link>
-            ))}
+            <div className="flex justify-center py-4 px-4">
+                <div className="flex flex-col justify-center items-center 
+                                h-full w-full max-w-3xl py-8  bg-blue-300">
+                    <Header/>
+                    {data.links.map((item, key) => (
+                        <Link linkItem={item}></Link>
+                    ))}
+                </div>
+            </div>
         </>
     );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,9 @@
 function Header() {
     return(
         <>
-            <h1>Test</h1>
+            <div className="block w-24 h-24 bg-black rounded-full"></div>
+            <h1>@test_user</h1>
+            <p>*Some sort of message here...</p>
         </>
     );
 }
