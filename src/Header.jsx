df041c192507d4f0dcab52224f33a7474299d2c4
Adrian Curammeng <adrianvcurammeng@gmail.com>
2024-11-03 01:03:38 -0500

    Feature: Links button basic layout
    - TODO: Decide on font sizes/font choice
    - TODO: Refine formatting

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <>
             <div className="block w-24 h-24 bg-black rounded-full"></div>
             <h1>@test_user</h1>
-            <p>*Some sort of message here...</p>
+            <p className="mb-4">*Some sort of message here...</p>
         </>
     );
 }
--- src/Link.jsx
+++ src/Link.jsx
@@ -3,8 +3,17 @@ import linksJson from './links.json'
 function Link({ linkItem }) {
     return(
         <>
-            <a href={linkItem.link}>{linkItem.name}</a>
-            <br></br>
+
+            <div className="w-full h-full bg-purple-500
+                            m-1 px-4">
+                <a className="w-full h-full p-2" href={linkItem.link}>
+                    <div className="flex flex-row justify-between">
+                        <p>logo</p>
+                        {linkItem.name}
+                        <p>logo</p>
+                    </div>
+                </a>
+            </div>
         </>
     )
 };
