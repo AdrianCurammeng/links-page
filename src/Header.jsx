dbf6c059cce8e31edb179a1ff516c2cac6ce66be
Adrian Curammeng <adrianvcurammeng@gmail.com>
2024-11-03 23:41:53 -0500

    Fix: Finalised fonts - Added 'Consolas' font - Fixed font sizes

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,8 +2,8 @@ function Header() {
     return(
         <>
             <div className="block w-24 h-24 bg-black rounded-full"></div>
-            <h1>@test_user</h1>
-            <p className="mb-4">*Some sort of message here...</p>
+            <h1 className="font-consolas text-2xl font-bold p-3">@test_user</h1>
+            <p className="font-consolas text-xl mb-4">*Some sort of message here...</p>
         </>
     );
 }
--- src/Link.jsx
+++ src/Link.jsx
@@ -3,10 +3,10 @@ import linksJson from './links.json'
 function Link({ linkItem }) {
     return(
         <>
-            <div className="w-full h-20 bg-green-500 my-2">
+            <div className="w-full h-20 bg-green-500 my-2 rounded-3xl">
                 <a className="flex flex-row justify-between h-full w-full p-6" href={linkItem.link} target="_blank">
                     <p>Icon</p>
-                    <p>{linkItem.name}</p>
+                    <p className="font-consolas text-xl">{linkItem.name}</p>
                     <p>Icon</p>
                 </a>
             </div>
