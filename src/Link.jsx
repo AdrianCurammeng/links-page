1ff13e87a002a647c49a9f3f585797b1d2fc80a1
Adrian Curammeng <adrianvcurammeng@gmail.com>
2024-11-04 11:58:55 -0500

    Refactor: Removed variables from Link component - Removed boolean and icon link variables

--- src/Link.jsx
+++ src/Link.jsx
@@ -1,14 +1,12 @@
 import linksJson from './links.json'
 
 function Link({ linkItem }) {
-    let logo_src = `https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/${linkItem.icon}.svg`;
-    let render_logo = linkItem.icon === "" ? false : true;  // checks if a simple-logo icon is rendered
     return(
         <>
             <div className="w-full h-20 bg-green-500 my-2 rounded-3xl">
                 <a className="flex flex-row justify-between h-full w-full p-6" href={linkItem.link} target="_blank">
-                    {render_logo ? <img height="32" width="32" src={logo_src} /> 
-                                 : <img height="32" width="32" src="../imgs/link.svg"/>}
+                    {linkItem.icon === "" ? <img height="32" width="32" src={'../imgs/link.svg'}/>
+										  : <img height="32" width="32" src={`https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/${linkItem.icon}.svg`}/>}
                     <p className="font-consolas text-xl">{linkItem.name}</p>
                     <p>Icon</p>
                 </a>
