1db37f5957f821dac0acf5c822a6551a659f5836
Adrian Curammeng <adrianvcurammeng@gmail.com>
2024-11-04 12:01:55 -0500

    Refactor: Fixed up formatting - Fixed React conditional formatting

--- src/Link.jsx
+++ src/Link.jsx
@@ -5,8 +5,7 @@ function Link({ linkItem }) {
         <>
             <div className="w-full h-20 bg-green-500 my-2 rounded-3xl">
                 <a className="flex flex-row justify-between h-full w-full p-6" href={linkItem.link} target="_blank">
-                    {linkItem.icon === "" ? <img height="32" width="32" src={'../imgs/link.svg'}/>
-										  : <img height="32" width="32" src={`https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/${linkItem.icon}.svg`}/>}
+                    {linkItem.icon === "" ? <img height="32" width="32" src={'../imgs/link.svg'}/> : <img height="32" width="32" src={`https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/${linkItem.icon}.svg`}/>}
                     <p className="font-consolas text-xl">{linkItem.name}</p>
                     <p>Icon</p>
                 </a>
