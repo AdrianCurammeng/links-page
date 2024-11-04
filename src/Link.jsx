2e6decb86d036c787a57b9150183bf0ef385b6da
Adrian Curammeng <adrianvcurammeng@gmail.com>
2024-11-04 00:12:22 -0500

    Feature: Add link icons - Added link icons for website buttons - Uses SimpleIcons icons - If no icon provided, a generic 'link' icon is used

--- src/Link.jsx
+++ src/Link.jsx
@@ -1,11 +1,14 @@
 import linksJson from './links.json'
 
 function Link({ linkItem }) {
+    let logo_src = `https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/${linkItem.icon}.svg`;
+    let render_logo = linkItem.icon === "" ? false : true;  // checks if a simple-logo icon is rendered
     return(
         <>
             <div className="w-full h-20 bg-green-500 my-2 rounded-3xl">
                 <a className="flex flex-row justify-between h-full w-full p-6" href={linkItem.link} target="_blank">
-                    <p>Icon</p>
+                    {render_logo ? <img height="32" width="32" src={logo_src} /> 
+                                 : <img height="32" width="32" src="../imgs/link.svg"/>}
                     <p className="font-consolas text-xl">{linkItem.name}</p>
                     <p>Icon</p>
                 </a>
